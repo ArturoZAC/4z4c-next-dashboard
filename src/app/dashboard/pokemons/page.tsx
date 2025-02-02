568bb353c91ec30222f0be8380821a53a01f561b
4z4c <arturoyz2105@gmail.com>
2025-02-01 21:36:05 -0500

    01-ssr

--- src/app/dashboard/pokemons/page.tsx
+++ src/app/dashboard/pokemons/page.tsx
@@ -1,11 +1,12 @@
-import { PokemonsResponse, SimplePokemon } from "@/app/pokemons";
-import Image from "next/image";
+import { PokemonGrid, PokemonsResponse, SimplePokemon } from "@/pokemons";
 
 const getPokemons = async( limit = 20, offset = 0): Promise<SimplePokemon[]> => {
   const data = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`);
   const pokemons: PokemonsResponse = await data.json();
   // console.log(pokemons);
 
+  // throw new Error('mal')
+  // throw notFound()
   return pokemons.results.map( poke => ({
     id: poke.url.split('/').at(-2)!,
     name: poke.name
@@ -19,19 +20,14 @@ export default async function PokemonsPage() {
 
   return (
     <div className="flex flex-col">
+      
+      <span className="text-5xl my-2">Listado de Pokemons <small> Estatico </small></span>
       {/* <h1>Hello PokemonsPage</h1> */}
       {/* {
         JSON.stringify( pokemons )
       } */}
+      <PokemonGrid pokemons={ pokemons }/>
 
-      <div className="flex flex-wrap gap-10 items-center justify-center">
-        <Image 
-          src={'https://raw.githubusercontent.com/PokeAPI/sprites/master/pokemon/other/dream-world/10.svg'}
-          width={100}
-          height={100}
-          alt="nombre"
-        />
-      </div>
     </div>
   );
 }
